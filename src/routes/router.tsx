e4e46bbce84a68a743eadd4aab5373436df49bbc
shuaib-code <shuaib.cyclist@gmail.com>
2025-01-02 00:56:45 +0600

    Theme added

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,20 @@
 import App from "@/App";
 import AuthLayout from "@/layouts/AuthLayout";
 import Login from "@/pages/Login";
+import { ThemeProvider } from "@/provider/themeProvider";
 import { Route, Routes } from "react-router";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/" element={<App />} />
 
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-      </Route>
-    </Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
