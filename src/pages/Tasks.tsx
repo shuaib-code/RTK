7e03750fc758aee1e938f6fd7d3f5446e056c5e4
shuaib-code <shuaib.cyclist@gmail.com>
2025-01-05 18:01:06 +0600

    No Task found UI added

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -5,6 +5,7 @@ import { taskSelector } from "@/redux/slices/task/taskSlice";
 
 export default function Task() {
   const tasks = useAppSelector(taskSelector);
+  if (tasks.length === 0) return <Nothing_UI />;
 
   return (
     <div className="px-2 mt-4">
@@ -16,6 +17,25 @@ export default function Task() {
         {tasks?.map((task, idx) => (
           <TaskCard task={task} key={idx} />
         ))}
+      </div>
+    </div>
+  );
+}
+
+function Nothing_UI() {
+  return (
+    <div className="px-2 mt-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold mb-6">My Tasks</h1>
+      </div>
+      <div className="grid gap-2">
+        <div className="flex flex-col items-center justify-center p-8 border rounded-xl text-center space-y-3  py-16">
+          <p className="text-base font-semibold leading-tight">No task found</p>
+          <p className="text-sm text-muted-foreground pb-10">
+            You don’t have any tasks yet. Start by adding a new task!
+          </p>
+          <AddTaskButton />
+        </div>
       </div>
     </div>
   );
--- src/redux/slices/task/taskSlice.tsx
+++ src/redux/slices/task/taskSlice.tsx
@@ -3,40 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { ITaskState } from "./types";
 
 const initialState = {
-  tasks: [
-    {
-      id: 1,
-      title: "Start next.js Course",
-      description: "This is a simple description of task",
-      dueDate: "2025-05",
-      isCompleted: false,
-      priority: "high",
-    },
-    {
-      id: 2,
-      title: "Finish Redux Class",
-      description: "This is a simple description of task",
-      dueDate: "2024-01",
-      isCompleted: false,
-      priority: "low",
-    },
-    {
-      id: 3,
-      title: "Complete Fronted NoSQL track",
-      description: "This is a simple description of task",
-      dueDate: "2023-01-23",
-      isCompleted: true,
-      priority: "medium",
-    },
-    {
-      id: 4,
-      title: "Finish SQL Track",
-      description: "This is a simple description of task",
-      dueDate: "2025-01-23",
-      isCompleted: false,
-      priority: "medium",
-    },
-  ],
+  tasks: [],
 } satisfies ITaskState;
 
 const taskSlice = createSlice({
