12830db4ca44fcd06047644e8a11234f0c563ad3
shuaib-code <shuaib.cyclist@gmail.com>
2025-01-07 21:29:46 +0600

    feat: gradient shadow for auth layout components

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,14 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div className=" min-h-screen flex items-center">
-      <Outlet />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="group relative w-[400px]">
+        <div className="absolute -inset-1 rounded-lg bg-gradient-to-tl from-[#1d4ed8] via-[#6b21a8] to-[#16a34a] opacity-55 blur transition duration-500 group-hover:opacity-65"></div>
+
+        <div className="relative rounded-lg">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
